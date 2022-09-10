46c2b0ed2f7b3a4214112f68507ccfb51059557f
Nicky <kolianpylupc@gmail.com>
2022-09-10 22:35:15 +0300

    add files

---
+++ js/index.js
@@ -0,0 +1,11 @@
+document.getElementById('movie-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    fetch("http://www.omdbapi.com/?apikey=1e8fb45a&t=Blade Runner")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            document.querySelector('.films').innerHTML = `
+                <img src="${data.Poster}" />
+            `
+        })
+})
